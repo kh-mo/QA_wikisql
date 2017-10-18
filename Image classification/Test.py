3240b91096cc884159c4658fee645f4e71b54013
kh-mo <momkdh2@naver.com>
2017-10-18 23:23:25 +0900

    init

--- Image classification/Test.py
+++ Image classification/Test.py
@@ -20,3 +20,4 @@ print("남성일 확률 : %g, 여성일 확률 : %g"%(prob[0][0] , prob[0][1]))
 
 sess.run(model.probability, {model.x: test_x, model.bol: bool})
 sess.run(model.fc3, {model.x: test_x, model.bol: bool})
+
