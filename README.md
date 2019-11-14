d79a8802e5211c444304bdc20150bc21595a38f0
kh-mo <momkdh2@naver.com>
2019-11-15 03:57:18 +0900

    init

--- README.md
+++ README.md
@@ -1,20 +1,10 @@
-# Requirement
+# QA with WikiSQL
 
-> * tensorflow 1.3.0
-> * python 3.5.2
+## Getting Start
+### Do EDA
+```shell
+python wikisqlEDA.py
+```
 
-# Deep-Learning
-
-1. Image Classification
-> * Block, Load_data, Model을 import하여 Train을 실행시키고 저장된 모델을 불러와 Test 실행.
-2. Text Summarization
-> * 진행 중....
-
-# Reference
-> * [Alexnet:ImageNet Classification with Deep Convolutional Neural Networks](https://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf)
-> * [ZFnet:Visualizaing and Understanding Convolutional Networks](https://www.cs.nyu.edu/~fergus/papers/zeilerECCV2014.pdf)
-> * [VGGnet:Very Deep Convolutional Networks for Large-Scale Image Recognition](https://arxiv.org/pdf/1409.1556.pdf)
-> * [Googlnet:Going Deeper with Convolutions](http://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=7298594)
-> * [Resnet:Deep Residual Learning for Image Recognition](http://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=7780459)
-> * [Densenet:Densely Connected Convolutional Networks](http://openaccess.thecvf.com/content_cvpr_2017/papers/Huang_Densely_Connected_Convolutional_CVPR_2017_paper.pdf)
-> * [Batch Normalization: Accelerating Deep Network Training by Reducing Internal Covariate Shift](http://proceedings.mlr.press/v37/ioffe15.pdf)
+## Reference
+- [1] [SEQ2SQL: GENERATING STRUCTURED QUERIES FROM NATURAL LANGUAGE USING REINFORCEMENT LEARNING](https://arxiv.org/pdf/1709.00103.pdf), arXiv 2017
---
+++ wikisqlEDA.py
@@ -0,0 +1,55 @@
+import os
+import json
+
+if __name__ == "__main__":
+    train_path = os.path.join(os.getcwd(), 'WikiSQL/data/train.jsonl')
+    dev_path = os.path.join(os.getcwd(), 'WikiSQL/data/dev.jsonl')
+    test_path = os.path.join(os.getcwd(), 'WikiSQL/data/test.jsonl')
+
+    with open(train_path) as tr, open(dev_path) as de, open(test_path) as ts:
+        train_data = tr.readlines()
+        dev_data = de.readlines()
+        test_data = ts.readlines()
+
+    print(len(train_data))
+    print(len(dev_data))
+    print(len(test_data))
+
+    tr_table = []
+    dev_table = []
+    ts_table = []
+
+    for doc in train_data:
+        tr_table.append(json.loads(doc)['table_id'])
+    for doc in dev_data:
+        dev_table.append(json.loads(doc)['table_id'])
+    for doc in test_data:
+        ts_table.append(json.loads(doc)['table_id'])
+
+    tr_table = list(set(tr_table))
+    dev_table = list(set(dev_table))
+    ts_table = list(set(ts_table))
+
+    print(len(tr_table))
+    print(len(dev_table))
+    print(len(ts_table))
+
+    print(len(train_data) + len(dev_data) + len(test_data))
+    print(len(tr_table) + len(dev_table) + len(ts_table))
+
+    list(set(tr_table) & set(dev_table))
+    list(set(tr_table) & set(ts_table))
+    list(set(dev_table) & set(ts_table))
+    '''
+    전체 학습 데이터 수 : 56,355
+    전체 검증 데이터 수 : 8,421
+    전체 테스트 데이터 수 : 15,878 
+    전체 : 80,654
+    
+    학습 테이블 수 : 17,984
+    검증 테이블 수 : 2,630
+    테스트 테이블 수 : 5,069
+    전체 : 25,683(페이퍼랑 다르다 - 더 많다?? 왜??)
+    겹치는 테이블은 없다... 근데 어떻게 안 본 테이블에서 정보를 얻지?
+    테이블 정보와 질문이 같이 들어간다는 가정
+    '''
