af7cf1a98b66513562998da0b2dc00f20727cce6
kh-mo <momkdh2@naver.com>
2019-12-04 01:45:22 +0900

    EDA in paper

--- wikisqlEDA.py
+++ wikisqlEDA.py
@@ -1,7 +1,9 @@
 import os
 import json
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    ## question information
     train_path = os.path.join(os.path.dirname(os.getcwd()), 'WikiSQL/data/train.jsonl')
     dev_path = os.path.join(os.path.dirname(os.getcwd()), 'WikiSQL/data/dev.jsonl')
     test_path = os.path.join(os.path.dirname(os.getcwd()), 'WikiSQL/data/test.jsonl')
@@ -13,7 +15,34 @@ if __name__ == "__main__":
 
     print("train lines : {}, dev lines : {}, test lines : {}".format(len(train_data), len(dev_data), len(test_data)))
     print("dataset total : {}".format(len(train_data+dev_data+test_data)))
-    # json.loads(train_data[0])
+
+    ## table information
+    tr_table = []
+    dev_table = []
+    ts_table = []
+    criterion = "table_id"
+
+    for doc in train_data:
+        tr_table.append(json.loads(doc)[criterion].split("-",maxsplit=1)[1])
+    for doc in dev_data:
+        dev_table.append(json.loads(doc)[criterion].split("-",maxsplit=1)[1])
+    for doc in test_data:
+        ts_table.append(json.loads(doc)[criterion].split("-",maxsplit=1)[1])
+
+    tr_table = set(tr_table)
+    dev_table = set(dev_table)
+    ts_table = set(ts_table)
+
+    print("train table lines : {}, dev table lines : {}, test table lines : {}".format(len(tr_table), len(dev_table), len(ts_table)))
+    print("tables total : {}".format(len(tr_table|dev_table|ts_table)))
+
+    ## plot
+    question_lengths = []
+    query_lengths = []
+    number_of_columns = []
+
+    for line in (train_data+dev_data+test_data):
+        question_lengths.append(len(json.loads(line)['question'].split(' ')))
 
     train_table_path = os.path.join(os.path.dirname(os.getcwd()), 'WikiSQL/data/train.tables.jsonl')
     dev_table_path = os.path.join(os.path.dirname(os.getcwd()), 'WikiSQL/data/dev.tables.jsonl')
@@ -24,62 +53,19 @@ if __name__ == "__main__":
         dev_table_data = de.readlines()
         test_table_data = ts.readlines()
 
-    # 어떤 정보가 table정보를 담고 있는 것인지??
-    tr_table = []
-    dev_table = []
-    ts_table = []
-    criterion = "id"
+    for line in (train_table_data + dev_table_data + test_table_data):
+        file = json.loads(line)
+        if file["id"].split("-",maxsplit=1)[1] in list(tr_table | dev_table | ts_table):
+            number_of_columns.append(len(file["header"]))
 
-    for doc in train_table_data:
-        tr_table.append(json.loads(doc)[criterion].rsplit("-",maxsplit=1)[0])
-    for doc in dev_table_data:
-        dev_table.append(json.loads(doc)[criterion].rsplit("-",maxsplit=1)[0])
-    for doc in test_table_data:
-        ts_table.append(json.loads(doc)[criterion].rsplit("-",maxsplit=1)[0])
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1, 2, 1) # row, col, index
+    ax2 = fig.add_subplot(1, 2, 2)
 
-    tr_table = set(tr_table)
-    dev_table = set(dev_table)
-    ts_table = set(ts_table)
+    ax1.hist(question_lengths, bins=list(range(60)), range=(0,60))
+    ax1.set_xlabel("Question lengths")
+    ax1.set_ylabel("Frequency")
 
-    print("train table lines : {}, dev table lines : {}, test table lines : {}".format(len(tr_table), len(dev_table), len(ts_table)))
-    print("tables total : {}".format(len(tr_table|dev_table|ts_table)))
-    len(set(tr_table) & set(dev_table))
-    len(set(tr_table) & set(ts_table))
-    len(set(dev_table) & set(ts_table))
-
-    len(tr_table | dev_table | ts_table - (tr_table&dev_table) - (dev_table&ts_table) - (ts_table&tr_table)|(dev_table&ts_table&tr_table))
-
-    len(tr_table & dev_table)
-    len(dev_table & ts_table)
-    len(ts_table&tr_table)
-    len(tr_table & dev_table & ts_table)
-
-    question_lengths = []
-    query_lengths = []
-    number_of_columns = []
-
-    for line in (train_data+dev_data+test_data):
-        question_lengths.append(len(json.loads(line)['question'].split(' ')))
-
-    for line in (tr_table+dev_table+ts_table):
-        question_lengths.append(len(json.loads(line)['question']))
-
-    len(train_table_data+dev_table_data+test_table_data)
-    import matplotlib.pyplot as plt
-    plt.hist(question_lengths, bins=list(range(60)), range=(0,60))
-
-    '''
-    전체 학습 데이터 수 : 56,355
-    전체 검증 데이터 수 : 8,421
-    전체 테스트 데이터 수 : 15,878 
-    전체 : 80,654
-    
-    학습 테이블 수 : 17,984
-    검증 테이블 수 : 2,630
-    테스트 테이블 수 : 5,069
-    전체 : 25,683(페이퍼랑 다르다 - 더 많다?? 왜??)
-    겹치는 테이블은 없다... 근데 어떻게 안 본 테이블에서 정보를 얻지?
-    테이블 정보와 질문이 같이 들어간다는 가정
-    '''
-
-    json.loads(train_data[0])["table_id"].rsplit("-",maxsplit=1)[0]
+    ax2.hist(number_of_columns, bins=list(range(40)), range=(0, 40))
+    ax2.set_xlabel("Number of columns")
+    ax2.set_ylabel("Frequency")
