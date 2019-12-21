a617296a646193faed153097e8fd0ed31db02011
kh-mo <momkdh2@naver.com>
2019-12-21 13:25:34 +0900

    add learn_bpe function

--- bpe.py
+++ bpe.py
@@ -1,4 +1,9 @@
+#-*- coding:utf-8 -*-
+
 import os
+import re
+import copy
+import argparse
 from collections import Counter, defaultdict
 
 def get_vocabulary(file):
@@ -21,7 +26,80 @@ def get_pair_statistics(file):
             prev_char = char
     return stats, indices
 
-def learn_bpe(infile, outfile):
+def replace_pair(pair, vocab, indices):
+    """('a','b')페어를 ('ab')페어로 바꾸기 위한 [문장위치, ('ab') ('a','b'), 빈도수] 리스트 반환 """
+    first, second = pair
+    pair_str = ''.join(pair)
+    pair_str = pair_str.replace('\\', '\\\\')
+    changes = []
+    pattern = re.compile(r'(?<!\S)' + re.escape(first + ' ' + second) + r'(?!\S)')
+    iterator = indices[pair].items()
+    for j, freq in iterator:
+        if freq < 1:
+            continue
+        word, freq = vocab[j]
+        new_word = ' '.join(word)
+        new_word = pattern.sub(pair_str, new_word)
+        new_word = tuple(new_word.split(' '))
+
+        vocab[j] = (new_word, freq)
+        changes.append((j, new_word, word, freq))
+    return changes
+
+def update_pair_statistics(pair, change, stats, indices):
+    ''' pair 정보를 stats와 indices에서 삭제, 합쳐진 pair에 대한 모든 정보를 change에서 받아와 '''
+    stats[pair] = 0
+    indices[pair] = defaultdict(int)
+    first, second = pair
+    new_pair = first + second
+    for j, word, old_word, freq in change:
+        # find all instances of pair, and update frequency/indices around it
+        i = 0
+        while True:
+            # find first symbol
+            try:
+                i = old_word.index(first, i)
+            except ValueError:
+                break
+            # if first symbol is followed by second symbol, we've found an occurrence of pair (old_word[i:i+2])
+            if i < len(old_word) - 1 and old_word[i + 1] == second:
+                # assuming a symbol sequence "A B C", if "B C" is merged, reduce the frequency of "A B"
+                if i:
+                    prev = old_word[i - 1:i + 1]
+                    stats[prev] -= freq
+                    indices[prev][j] -= 1
+                if i < len(old_word) - 2:
+                    # assuming a symbol sequence "A B C B", if "B C" is merged, reduce the frequency of "C B".
+                    # however, skip this if the sequence is A B C B C, because the frequency of "C B" will be reduced by the previous code block
+                    if old_word[i + 2] != first or i >= len(old_word) - 3 or old_word[i + 3] != second:
+                        nex = old_word[i + 1:i + 3]
+                        stats[nex] -= freq
+                        indices[nex][j] -= 1
+                i += 2
+            else:
+                i += 1
+
+        i = 0
+        while True:
+            try:
+                # find new pair
+                i = word.index(new_pair, i)
+            except ValueError:
+                break
+            # assuming a symbol sequence "A BC D", if "B C" is merged, increase the frequency of "A BC"
+            if i:
+                prev = word[i - 1:i + 1]
+                stats[prev] += freq
+                indices[prev][j] += 1
+            # assuming a symbol sequence "A BC B", if "B C" is merged, increase the frequency of "BC B"
+            # however, if the sequence is A BC BC, skip this step because the count of "BC BC" will be incremented by the previous code block
+            if i < len(word) - 1 and word[i + 1] != new_pair:
+                nex = word[i:i + 2]
+                stats[nex] += freq
+                indices[nex][j] += 1
+            i += 1
+
+def learn_bpe(infile, outfile, min_frequency, num_symbols, total_symbols, verbose):
     # 단어의 마지막 토큰을 "</w>"로 지정한 버전입니다.
     outfile.write('#version: 0.2\n')
 
@@ -32,17 +110,53 @@ def learn_bpe(infile, outfile):
     # 단어를 count 기준으로 내림차순 정렬합니다.
     sorted_vocab = sorted(vocab.items(), key=lambda x:x[1], reverse=True)
 
-    # 이건 뭐고 왜 하는거지???
-    state, indices = get_pair_statistics(sorted_vocab)
+    # stats : {[token1, token2]:count}, indices {[token1,token2]:{1번단어:count, 2번단어:count}}
+    # stats는 특정 단어 페어가 등장한 총 수, indices는 해당 단어 페어가 등장한 위치
+    stats, indices = get_pair_statistics(sorted_vocab)
+
+    # 개별 유닛수를 구해 전체 단어 수에서 제외
+    if total_symbols:
+        uniq_char_internal = set()
+        uniq_char_final = set()
+        for word in vocab:
+            for char in word[:-1]:
+                uniq_char_internal.add(char)
+            uniq_char_final.add(word[-1])
+        num_symbols -= len(uniq_char_internal) + len(uniq_char_final)
+
+    for i in range(num_symbols):
+        # 가장 빈도가 높은 pair 획득
+        if stats:
+            most_frequent = max(stats, key=lambda x: stats[x])
+
+        # 모든 pair들이 최소 빈도수 이하일 경우 종료
+        if stats[most_frequent] < min_frequency:
+            print('no pair has frequency >= {0}. Stopping\n'.format(min_frequency))
+            break
+
+        # 진행상황 보여주기
+        if verbose:
+            print('pair {0}: {1} {2} -> {1}{2} (frequency {3})\n'.format(
+                i, most_frequent[0], most_frequent[1], stats[most_frequent]))
+
+        outfile.write('{0} {1}\n'.format(*most_frequent))
+        changes = replace_pair(most_frequent, sorted_vocab, indices)
+        update_pair_statistics(most_frequent, changes, stats, indices)
+
     outfile.close()
 
 def apply_bpe():
     return
 
 if __name__ == "__main__":
-    infile = open(os.path.join(os.getcwd(), "preprocess/train.txt"), "r")
-    outfile = open(os.path.join(os.getcwd(), "preprocess/codes.txt"), "w")
-    learn_bpe(infile, outfile)
+    infile = open(os.path.join(os.getcwd(), "preprocess/train.txt"), "r", encoding="utf-8")
+    outfile = open(os.path.join(os.getcwd(), "preprocess/codes.txt"), "w", encoding="utf-8")
+    resultfile = open(os.path.join(os.getcwd(), "preprocess/train_bpe.txt"), "w", encoding="utf-8")
+    num_symbols = 10000 # 총 단어 수
+    total_symbols = False # 총 단어 수에 개별 유닛(a,b,c 등) 포함 여부
+    verbose = False # 진행경과를 보여줄 지 여부
+    min_frequency = 2 # 종료 최소 빈도 수(등장한 유닛 pair들이 모두 이 기준보다 작다면 종료)
+    learn_bpe(infile, outfile, min_frequency, num_symbols, total_symbols, verbose)
     apply_bpe()
 
 
