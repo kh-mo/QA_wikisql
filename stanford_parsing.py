c43f64fa01245f3579a07bd884b4b4d4c311311a
kh-mo <momkdh2@naver.com>
2019-12-29 12:17:30 +0900

    type 유형 추가, 주석 정리

--- stanford_parsing.py
+++ stanford_parsing.py
@@ -13,53 +13,55 @@ import corenlp
 # [t.lemma for t in sentence.token]
 
 if __name__ == "__main__":
-    # read train question, header file
-    type = "train"
-    path = os.path.join(os.path.dirname(os.getcwd()), "WikiSQL/data/" + type + ".jsonl")
-    table_path = os.path.join(os.path.dirname(os.getcwd()), "WikiSQL/data/" + type + ".tables.jsonl")
+    # read type question, header file
+    types = ["train","dev","test"]
 
-    qs = open(path).readlines()
-    ts = open(table_path).readlines()
+    for type in types:
+        path = os.path.join(os.path.dirname(os.getcwd()), "WikiSQL/data/" + type + ".jsonl")
+        table_path = os.path.join(os.path.dirname(os.getcwd()), "WikiSQL/data/" + type + ".tables.jsonl")
 
-    # corenlp parser
-    client = corenlp.CoreNLPClient(annotators="tokenize ssplit pos lemma ner depparse".split())
+        qs = open(path).readlines() # question sentence
+        ts = open(table_path).readlines() # table sentence
 
-    # make preprocess folder
-    preprocessed_dir = os.path.join(os.getcwd(), "preprocess")
-    if not os.path.isdir(preprocessed_dir):#폴더 없으면 생성 있으면 패스
-        os.mkdir(preprocessed_dir)
+        # corenlp parser
+        client = corenlp.CoreNLPClient(annotators="tokenize ssplit pos lemma ner depparse".split())
 
-    # tokenizing
-    with open(os.path.join(preprocessed_dir, type + ".txt"), "w", encoding="utf-8") as w:
-        try:
-            for line in qs:
-                ann = client.annotate(json.loads(line)["question"])
-                # corenlp로 파싱된 question 단어들을 space로 분할하여 한 문장으로 저장
-                w.write(" ".join([t.word for t in ann.sentence[0].token]))
-                w.write("\n")
-            for line in ts:
-                for word in json.loads(line)["header"]:
-                    ann = client.annotate(word)
-                    # corenlp로 파싱된 header 단어들을 space로 분할하여 한 문장으로 저장
+        # make preprocess folder
+        preprocessed_dir = os.path.join(os.getcwd(), "preprocess")
+        if not os.path.isdir(preprocessed_dir): # 폴더 없으면 생성 있으면 패스
+            os.mkdir(preprocessed_dir)
+
+        # tokenizing
+        with open(os.path.join(preprocessed_dir, type + ".txt"), "w", encoding="utf-8") as w:
+            try:
+                for line in qs:
+                    ann = client.annotate(json.loads(line)["question"])
+                    # corenlp로 파싱된 question 단어들을 space로 분할하여 한 문장으로 저장
                     w.write(" ".join([t.word for t in ann.sentence[0].token]))
                     w.write("\n")
-        except IndexError as e:
-            pass
-    print("word tokenizing done.")
+                for line in ts:
+                    for word in json.loads(line)["header"]:
+                        ann = client.annotate(word)
+                        # corenlp로 파싱된 header 단어들을 space로 분할하여 한 문장으로 저장
+                        w.write(" ".join([t.word for t in ann.sentence[0].token]))
+                        w.write("\n")
+            except IndexError as e:
+                pass
+        print("word tokenizing done.")
 
-    with open(os.path.join(preprocessed_dir, type + "_lemma.txt"), "w", encoding="utf-8") as w:
-        try:
-            for line in qs:
-                ann = client.annotate(json.loads(line)["question"])
-                # corenlp로 파싱된 question 단어들을 space로 분할하여 한 문장으로 저장
-                w.write(" ".join([t.lemma for t in ann.sentence[0].token]))
-                w.write("\n")
-            for line in ts:
-                for word in json.loads(line)["header"]:
-                    ann = client.annotate(word)
-                    # corenlp로 파싱된 header 단어들을 space로 분할하여 한 문장으로 저장
+        with open(os.path.join(preprocessed_dir, type + "_lemma.txt"), "w", encoding="utf-8") as w:
+            try:
+                for line in qs:
+                    ann = client.annotate(json.loads(line)["question"])
+                    # corenlp로 파싱된 question 단어들을 space로 분할하여 lemmatization 문장으로 저장
                     w.write(" ".join([t.lemma for t in ann.sentence[0].token]))
                     w.write("\n")
-        except IndexError as e:
-            pass
-    print("lemma tokenizing done.")
+                for line in ts:
+                    for word in json.loads(line)["header"]:
+                        ann = client.annotate(word)
+                        # corenlp로 파싱된 header 단어들을 space로 분할하여 lemmatization 문장으로 저장
+                        w.write(" ".join([t.lemma for t in ann.sentence[0].token]))
+                        w.write("\n")
+            except IndexError as e:
+                pass
+        print("lemma tokenizing done.")
