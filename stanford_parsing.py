5b5fe6e1854b85b8931ca24de82924198a2c8336
kh-mo <momkdh2@naver.com>
2020-01-13 09:58:38 +0900

    . 이후 token이 사라지는 문제 해결

--- stanford_parsing.py
+++ stanford_parsing.py
@@ -4,13 +4,21 @@ import os
 import json
 import corenlp
 # os.getenv("CORENLP_HOME") # 환경변수가 잘 지정되었는지 확인하는 코드
-# assert corenlp.to_text(sentence) == text
-# text = "test document"
-# with corenlp.CoreNLPClient(annotators="tokenize ssplit pos lemma ner depparse".split()) as client:
-#     ann = client.annotate(text)
-# sentence = ann.sentence[0]
-# [t.word for t in sentence.token]
-# [t.lemma for t in sentence.token]
+
+def replace_origin_preprocessing(word):
+    # 공백 대체 : replace(u'\xa0', u' ')
+    # 큰 따옴표가 stanford parser에서 원하는대로 변환되지 않아 작은따옴표 두개로 대체 : replace("\"", "\'\'")
+    return word.replace(u'\xa0', u' ').replace("\"", "``").strip()
+
+def replace_tokenizing_preprocessing(word):
+    # 공백 대체 : replace(u'\xa0', u' ')
+    # 큰 따옴표가 stanford parser에서 원하는대로 변환되지 않아 작은따옴표 두개로 대체 : replace("\"", "\'\'")
+    # 본래 공백을 __로 대체 : replace(" ", "__")
+    return word.replace(u'\xa0', u' ').replace("\"", "``").replace(" ", "__")
+
+def write_tokening_preprocessing(word):
+    # parsing 이후 "__ "을 "__"로 대체하여 본래 띄어쓰기가 있었던 지점을 표현한다(추후 이것을 이용해 원래 문장으로 복원한다)
+    return word.replace("__ ", "__").replace("--", "-").strip()
 
 if __name__ == "__main__":
     # read type question, header file
@@ -35,12 +43,11 @@ if __name__ == "__main__":
         with open(os.path.join(preprocessed_dir, type + ".txt"), "w", encoding="utf-8") as w:
             try:
                 for line in qs:
-                    q = json.loads(line)["question"]
-                    w.write(q)
+                    w.write(replace_origin_preprocessing(json.loads(line)["question"]))
                     w.write("\n")
                 for line in ts:
                     for word in json.loads(line)["header"]:
-                        w.write(word)
+                        w.write(replace_origin_preprocessing(word))
                         w.write("\n")
             except IndexError as e:
                 pass
@@ -49,16 +56,16 @@ if __name__ == "__main__":
         # tokenizing
         with open(os.path.join(preprocessed_dir, type + "_token.txt"), "w", encoding="utf-8") as w:
             try:
-                for idx, line in enumerate(qs):
-                    ann = client.annotate(json.loads(line)["question"].replace(u'\xa0', u' ').replace(" ", "__"))
+                for line in qs:
+                    ann = client.annotate(replace_tokenizing_preprocessing(json.loads(line)["question"]))
                     # corenlp로 파싱된 question 단어들을 space로 분할하여 한 문장으로 저장
-                    w.write(" ".join([t.word for t in ann.sentence[0].token]).replace("__ ", "__"))
+                    w.write(write_tokening_preprocessing(" ".join([t.word for sent in ann.sentence for t in sent.token])))
                     w.write("\n")
                 for line in ts:
                     for word in json.loads(line)["header"]:
-                        ann = client.annotate(word.replace(u'\xa0', u' ').replace(" ", "__"))
+                        ann = client.annotate(replace_tokenizing_preprocessing(word))
                         # corenlp로 파싱된 header 단어들을 space로 분할하여 한 문장으로 저장
-                        w.write(" ".join([t.word for t in ann.sentence[0].token]).replace("__ ", "__"))
+                        w.write(write_tokening_preprocessing(" ".join([t.word for sent in ann.sentence for t in sent.token])))
                         w.write("\n")
             except IndexError as e:
                 pass
@@ -67,16 +74,18 @@ if __name__ == "__main__":
         with open(os.path.join(preprocessed_dir, type + "_lemma.txt"), "w", encoding="utf-8") as w:
             try:
                 for line in qs:
-                    ann = client.annotate(json.loads(line)["question"].replace(" ", "__"))
+                    ann = client.annotate(replace_tokenizing_preprocessing(json.loads(line)["question"]))
                     # corenlp로 파싱된 question 단어들을 space로 분할하여 lemmatization 문장으로 저장
-                    w.write(" ".join([t.lemma for t in ann.sentence[0].token]).replace("__ ", "__"))
+                    w.write(write_tokening_preprocessing(" ".join([t.lemma for sent in ann.sentence for t in sent.token])))
                     w.write("\n")
                 for line in ts:
                     for word in json.loads(line)["header"]:
-                        ann = client.annotate(word.replace(" ", "__"))
+                        ann = client.annotate(replace_tokenizing_preprocessing(word))
                         # corenlp로 파싱된 header 단어들을 space로 분할하여 lemmatization 문장으로 저장
-                        w.write(" ".join([t.lemma for t in ann.sentence[0].token]).replace("__ ", "__"))
+                        w.write(write_tokening_preprocessing(" ".join([t.lemma for sent in ann.sentence for t in sent.token])))
                         w.write("\n")
             except IndexError as e:
                 pass
         print("word {} lemmatization done.".format(type))
+
+
