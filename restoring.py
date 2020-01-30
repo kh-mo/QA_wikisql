1b89b53a703e01ed70c1137ec8d5f6a8434d2c52
skmkh19 <skmkh19@gmail.com>
2020-01-30 15:11:27 +0900

    json input

--- restoring.py
+++ restoring.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8 -*-
 
 import os
+import json
 import argparse
 
 def set_hyperparameters():
@@ -16,9 +17,6 @@ def recover_bpe(line):
 
 def recover_tokenizer(line):
     sent = line.strip()
-    sent = sent.replace("-LRB-", "(").replace("-RRB-", ")")
-    sent = sent.replace("-LSB-", "[").replace("-RSB-", "]")
-    sent = sent.replace("-LCB-", "{").replace("-RCB-", "}")
     sent = sent.replace("Jr..", "Jr.").replace("Seq..", "Seq.")
     sent = sent.replace("etc..", "etc.").replace("Etc..", "Etc..").replace("est..","est.").replace("Est..", "Est.")
     sent = sent.replace("Jan..", "Jan.").replace("Feb..", "Feb.").replace("Mar..", "Mar.").replace("Apr..", "Apr.")
@@ -32,16 +30,24 @@ if __name__ == "__main__":
 
     for type in types:
         if args.use_bpe == "True":
-            infile = open(os.path.join(os.getcwd(), "preprocess/" + type + "_bpe_" + str(args.merges) + ".txt"), 'r', encoding='utf-8')
-            outfile = open(os.path.join(os.getcwd(), "preprocess/" + type + "_restore_bpe_" + str(args.merges) + ".txt"), 'w', encoding='utf-8')
+            infile = open(os.path.join(os.getcwd(), "preprocess/" + type + "_bs_" + str(args.merges) + ".jsonl"), 'r', encoding='utf-8')
+            outfile = open(os.path.join(os.getcwd(), "preprocess/" + type + "_bs_" + str(args.merges) + "_restore.jsonl"), 'w', encoding='utf-8')
         else:
-            infile = open(os.path.join(os.getcwd(), "preprocess/" + type + "_token_basic.txt"), 'r', encoding='utf-8')
-            outfile = open(os.path.join(os.getcwd(), "preprocess/" + type + "_restore_token_basic.txt"), 'w', encoding='utf-8')
+            infile = open(os.path.join(os.getcwd(), "preprocess/" + type + "_bs.jsonl"), 'r', encoding='utf-8')
+            outfile = open(os.path.join(os.getcwd(), "preprocess/" + type + "_bs_restore.jsonl"), 'w', encoding='utf-8')
 
         for line in infile:
-            outfile.write(recover_tokenizer(recover_bpe(line)))
-            outfile.write("\n")
-            # outfile.write(recover_tokenizer(recover_bpe(line)))
+            doc = json.loads(line)
+            if args.use_bpe == "True":
+                doc['nli_restore'] = recover_tokenizer(recover_bpe(doc['nli_bs']))
+                doc['col_restore'] = []
+                for word in doc['col_bs']:
+                    doc['col_restore'].append(recover_tokenizer(recover_bpe(word)))
+            else:
+                doc['nli_restore'] = recover_tokenizer(recover_bpe(doc['nli_b']))
+                doc['col_restore'] = []
+                for word in doc['col_b']:
+                    doc['col_restore'].append(recover_tokenizer(recover_bpe(word)))
 
-    # infile = open(args.infile, 'r', encoding='utf-8')
-    # outfile = open(args.outfile, 'w', encoding='utf-8')
+            outfile.write(json.dumps(doc, ensure_ascii=False))
+            outfile.write("\n")
